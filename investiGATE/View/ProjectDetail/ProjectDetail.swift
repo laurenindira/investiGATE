b1af4e941fe29cf02f1d303a7affcc9498737550
raihanaza <86710982+raihanaza@users.noreply.github.com>
2025-03-15 15:42:14 -0700

    added missing curly brace

---
+++ investiGATE/View/ProjectDetail/ProjectDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectDetail.swift
+//  investiGATE
+//
+//  Created by Raihana Zahra on 3/15/25.
+//
+
+import SwiftUI
+
+struct ProjectDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectDetail()
+}
