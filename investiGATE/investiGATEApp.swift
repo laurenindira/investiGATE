bcacb8ffe3ad309220dbad5d6be68f4abc6448b3
laurenindira <50187316+laurenindira@users.noreply.github.com>
2025-03-15 10:34:09 -0700

    Initial Commit

---
+++ investiGATE/investiGATEApp.swift
@@ -0,0 +1,17 @@
+//
+//  investiGATEApp.swift
+//  investiGATE
+//
+//  Created by Lauren Indira on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct investiGATEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
