e8f4fc5da60e6000b03b3fb6e5b7bd9f1a61b79e
raihanaza <86710982+raihanaza@users.noreply.github.com>
2025-03-15 14:28:14 -0700

    adding project cards, project struct from cecilia

---
+++ investiGATE/Model/Project.swift
@@ -0,0 +1,8 @@
+//
+//  Project.swift
+//  investiGATE
+//
+//  Created by Raihana Zahra on 3/15/25.
+//
+
+import Foundation
---
+++ investiGATE/View/Dashboard/Tag.swift
@@ -0,0 +1,18 @@
+//
+//  Tag.swift
+//  investiGATE
+//
+//  Created by Raihana Zahra on 3/15/25.
+//
+
+import SwiftUI
+
+struct Tag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tag()
+}
